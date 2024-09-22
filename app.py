b50d1f3858861ae0b8b62701de70bd28ad5863cc
bruniin <brunovinicius2002@hotmail.com>
2024-09-21 21:25:06 -0300

    Testing

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from resources.Employee import Employee
 app = Flask(__name__)
 api = Api(app)
 
-    
 api.add_resource(Employee, "/employee", endpoint="buscar", methods=["GET"])
 api.add_resource(Employee, "/employee", endpoint="incluir", methods=["POST"])
 api.add_resource(Employee, "/employee/<int:id>", endpoint="alterar", methods=["PUT"])
